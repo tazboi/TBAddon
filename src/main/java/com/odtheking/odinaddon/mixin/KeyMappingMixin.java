e8acd23ec65a5b9146411225633d2893e96bf775
tazboi <cluby10000@gmail.com>
2026-01-27 23:34:11 -0700

    Added a system to ensure that ItemSwaps on non-isDown checks.

--- src/main/java/com/odtheking/odinaddon/mixin/KeyMappingMixin.java
+++ src/main/java/com/odtheking/odinaddon/mixin/KeyMappingMixin.java
@@ -3,12 +3,14 @@ package com.odtheking.odinaddon.mixin;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.odtheking.odinaddon.features.impl.skyblock.event.InputReleaseEvent;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(KeyMapping.class)
 public class KeyMappingMixin {
@@ -18,6 +20,9 @@ public class KeyMappingMixin {
     @Shadow
     protected InputConstants.Key key;
 
+    @Shadow
+    private int clickCount;
+
     @Inject(
             method = "setDown",
             at = @At("HEAD"),
@@ -26,5 +31,19 @@ public class KeyMappingMixin {
     private void preRelease(boolean bl, CallbackInfo ci) {
         if (!this.isDown || bl) return;
         if (new InputReleaseEvent(this.key).postAndCatch()) ci.cancel();
+    }
+
+    @Inject(
+            method = "consumeClick",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/KeyMapping;clickCount:I",
+                    opcode = org.objectweb.asm.Opcodes.PUTFIELD,
+                    shift = At.Shift.AFTER
+            )
+    )
+    private void onClick(CallbackInfoReturnable<Boolean> cir) {
+
+        if (this.clickCount == 0 && !this.isDown) new InputReleaseEvent(this.key).postAndCatch();
     }
 }
