2bfe20946fde3ed0ca1e0b3a002856d07e5345e7
tazboi <cluby10000@gmail.com>
2026-06-16 14:22:25 -0700

    ItemRarity non-rarity labelled item customization (requested)

--- src/main/kotlin/com/odtheking/odinaddon/features/impl/render/ItemColor.kt
+++ src/main/kotlin/com/odtheking/odinaddon/features/impl/render/ItemColor.kt
@@ -1,5 +1,7 @@
 package com.odtheking.odinaddon.features.impl.render
 
+import com.odtheking.odin.clickgui.settings.Setting.Companion.withDependency
+import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
 import com.odtheking.odin.clickgui.settings.impl.NumberSetting
 import com.odtheking.odin.events.GuiEvent
 import com.odtheking.odin.events.core.on
@@ -12,6 +14,8 @@ import com.odtheking.odin.utils.getSkyblockRarity
 import com.odtheking.odin.utils.loreString
 import com.odtheking.odin.utils.matchesOneOf
 import com.odtheking.odin.utils.modMessage
+import com.odtheking.odin.utils.skyblock.LocationUtils
+import com.odtheking.odin.utils.skyblock.SkyblockPlayer
 import com.odtheking.odinaddon.features.impl.skyblock.event.HotbarSlotRenderEvent
 import com.odtheking.odinaddon.utils.findFirstTextWithColor
 import net.minecraft.ChatFormatting
@@ -33,15 +37,16 @@ object ItemColor : Module(
         0.05,
         desc = "See-through ability of the rarity background."
     )
-    private val rarityTextColorSet: Set<TextColor> =
-        ItemRarity.entries.mapNotNull { rarity ->
-            ChatFormatting.getByCode(rarity.colorCode.last())?.let {
-                TextColor.fromLegacyFormat(it)
-            }
-        }.toSet()
+    private val renderNonRarity by BooleanSetting("Render Non-rarity Items", desc = "Whether to render item rarity on items without an explicit rarity in lore.", default = false)
+    private val commonIncluded by BooleanSetting("Non-rarity Commons", desc = "Whether to render items without an explicit rarity that are \"common.\" Disable/reenable module to apply setting.", default = true).withDependency { renderNonRarity }
+
+
+    private var rarityTextColorSet: Set<TextColor> = mapRarities(commonIncluded)
 
     init {
         on<GuiEvent.RenderSlot> {
+            if (!LocationUtils.isInSkyblock) return@on
+
             if ("Catacombs" in screen.title.string ||
                 "Kuudra" in screen.title.string) {
                 return@on
@@ -59,6 +64,7 @@ object ItemColor : Module(
 
 
         on<HotbarSlotRenderEvent> {
+            if (!LocationUtils.isInSkyblock) return@on
             if (item.isEmpty) return@on
 
             val rarityColor = getSkyblockRarity(item.loreString)?.color ?: return@on
@@ -67,9 +73,25 @@ object ItemColor : Module(
     }
 
     private fun resolveRarityFromName(item: ItemStack): Int? {
-        val name = item.displayName
+        if (!renderNonRarity) return null
 
+        val name = item.displayName
         return findFirstTextWithColor(name, rarityTextColorSet)?.component2()?.value
+    }
+
+    private fun mapRarities(commonIncluded: Boolean = false): Set<TextColor> {
+        return ItemRarity.entries.mapNotNull { rarity ->
+            if (!commonIncluded && rarity == ItemRarity.COMMON) return@mapNotNull null
+
+            ChatFormatting.getByCode(rarity.colorCode.last())?.let {
+                TextColor.fromLegacyFormat(it)
+            }
+        }.toSet()
+    }
+
+    override fun onEnable() {
+        super.onEnable()
+        rarityTextColorSet = mapRarities(commonIncluded)
     }
 
 
